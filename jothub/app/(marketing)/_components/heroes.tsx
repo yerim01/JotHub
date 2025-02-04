7a626a05f4d0354c7f16deef4ffc53655c09de46
Yerim Moon <myr9901@gmail.com>
2025-02-03 17:21:01 -0800

    updated images and background color on landing page for dark mode

--- jothub/app/(marketing)/_components/heroes.tsx
+++ jothub/app/(marketing)/_components/heroes.tsx
@@ -4,11 +4,17 @@ export const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
-        <div className="relative w-[300px] h-[300px] sm:w-[300px] sm:h-[300px] md:h-[400px] md:w-[400px]">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
           <Image
             src="/documents.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Documents"
+          />
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Documents"
           />
         </div>
@@ -16,7 +22,13 @@ export const Heroes = () => {
           <Image
             src="/reading.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Reading"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Reading"
           />
         </div>
