2c8394e317c5fe3e0b46c3daaf0d3fd4167fee4d
Yerim Moon <myr9901@gmail.com>
2025-07-08 16:11:01 -0700

    updated a user dropdown in the sidebar and enabled logout

--- jothub/app/(main)/_components/navigation.tsx
+++ jothub/app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -114,7 +115,7 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
