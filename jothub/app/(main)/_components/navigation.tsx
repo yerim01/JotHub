4f30ac611a7bb6911afdde2bd1b26f242c3f8b01
Yerim Moon <myr9901@gmail.com>
2025-07-15 16:19:13 -0700

    feature: created a search hook using zustand. search can be triggered either by clicking the search box in the sidebar or by pressing command keys

--- jothub/app/(main)/_components/navigation.tsx
+++ jothub/app/(main)/_components/navigation.tsx
@@ -22,6 +22,7 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@/components/ui/popover";
+import { useSearch } from "@/hooks/use-search";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
@@ -29,6 +30,7 @@ import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -147,7 +149,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
-          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
           <Item label="Settings" icon={Settings} onClick={() => {}} />
           <Item label="New page" icon={PlusCircle} onClick={handleCreate} />
         </div>
