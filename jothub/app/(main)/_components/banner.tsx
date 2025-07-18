772e660cae098c365b95f51a5badcad5ae021a1a
Yerim Moon <myr9901@gmail.com>
2025-07-17 20:25:23 -0700

    feat: enable real time updates for note titles.

---
+++ jothub/app/(main)/_components/banner.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+
+interface BannerProps {
+  documentId: Id<"documents">;
+}
+
+export const Banner = ({ documentId }: BannerProps) => {
+  const router = useRouter();
+
+  const remove = useMutation(api.documents.remove);
+  const restore = useMutation(api.documents.restore);
+
+  
+  return <div>Banner</div>;
+};
