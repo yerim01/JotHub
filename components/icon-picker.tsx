7f16c653ea99f73de41f4211cfee12d5b4cb994b
Yerim Moon <myr9901@gmail.com>
2025-07-27 23:13:45 -0700

    style: changed emoji picker z index to appear above the editor

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -31,15 +31,17 @@ export const IconPicker = ({
   const theme = themeMap[currentTheme];
 
   return (
-    <Popover>
-      <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
-      <PopoverContent className="p-0 w-full border-none shadow-none">
-        <EmojiPicker
-          height={350}
-          theme={theme}
-          onEmojiClick={(data) => onChange(data.emoji)}
-        />
-      </PopoverContent>
-    </Popover>
+    <div className="z-[99999]">
+      <Popover>
+        <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
+        <PopoverContent className="p-0 w-full border-none shadow-none">
+          <EmojiPicker
+            height={350}
+            theme={theme}
+            onEmojiClick={(data) => onChange(data.emoji)}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 };
